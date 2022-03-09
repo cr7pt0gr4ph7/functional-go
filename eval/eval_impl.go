d937a265ca866f10efce26632a46a37cdcb9cb30
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-09 14:46:54 +0100

    Update eval_impl.go

--- eval/eval_impl.go
+++ eval/eval_impl.go
@@ -21,13 +21,58 @@ type evalImpl[A any] interface {
 	Memoize() Eval[A]
 }
 
-func fromImpl[A any, I evalImpl[A]](impl I) Eval[A] {
-	return Eval[A]{impl: impl}
-}
-
+func (n *nowImpl[A]) kind() evalKind        { return kNow }
+func (l *laterImpl[A]) kind() evalKind      { return kLater }
+func (a *alwaysImpl[A]) kind() evalKind     { return kAlways }
 func (d *deferImpl[A]) kind() evalKind      { return kDefer }
 func (f *flatMapImpl[S, A]) kind() evalKind { return kFlatMap }
 func (m *memoizeImpl[A]) kind() evalKind    { return kMemoize }
+
+// nowImpl provides the implementation for `Now()`.
+type nowImpl[A any] struct {
+	run func() Eval[A]
+}
+
+func (d *nowImpl[A]) Value() A {
+	return d.run().Value()
+}
+
+func (d *nowImpl[A]) Memoize() Eval[A] {
+	return wrapWithMemoize(fromImpl[A](d))
+}
+
+// laterImpl provides the implementation for `Later()`.
+type laterImpl[A any] struct {
+	provider func() A
+	result   option.Optional[A]
+}
+
+func (l *laterImpl[A]) Value() A {
+	if v, ok := l.result.Value(); ok {
+		return v
+	} else {
+		r := l.provider()
+		l.result = option.Some(r)
+		return r
+	}
+}
+
+func (l *laterImpl[A]) Memoize() Eval[A] {
+	return fromImpl[A](l)
+}
+
+// alwaysImpl provides the implementation for `Always()`.
+type alwaysImpl[A any] struct {
+	provider func() A
+}
+
+func (a *alwaysImpl[A]) Value() A {
+	return a.provider()
+}
+
+func (a *alwaysImpl[A]) Memoize() Eval[A] {
+	return fromImpl[A](&laterImpl[A]{provider: a.provider})
+}
 
 // deferImpl provides the implementation for `Defer()`.
 type deferImpl[A any] struct {
@@ -62,6 +107,10 @@ type memoizeImpl[A any] struct {
 	result option.Optional[A]
 }
 
+func fromImpl[A any, I evalImpl[A]](impl I) Eval[A] {
+	return Eval[A]{impl: impl}
+}
+
 func wrapWithMemoize[A any](eval Eval[A]) Eval[A] {
 	return fromImpl[A](&memoizeImpl[A]{eval: eval})
 }
@@ -74,7 +123,6 @@ func (m *memoizeImpl[A]) Value() A {
 		m.result = option.Some(r)
 		return r
 	}
-
 }
 
 func (m *memoizeImpl[A]) Memoize() Eval[A] {
