579d6da65773bf15633f76784363a25e910a67b4
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-09 11:16:11 +0100

    Create slice_test.go

---
+++ slice_test.go
@@ -0,0 +1,16 @@
+package functional
+
+func ExampleSlice_FoldLeft() {
+	s := Slice[int]{1, 2, 3, 4}
+  r := FoldLeft(s, 10, func(elem int, state int) int {
+		fmt.Println(elem, state)
+		return elem + state
+	})
+	fmt.Println("=", r)
+	// Output:
+	// 1 10
+	// 2 11
+	// 3 13
+	// 4 16
+	// = 20
+}
