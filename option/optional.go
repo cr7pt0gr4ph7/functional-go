28564a6194c4c432b918d435e3447859bbd7fcc1
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-11 21:52:15 +0100

    Update optional.go

--- option/optional.go
+++ option/optional.go
@@ -13,6 +13,22 @@ func None[T any]() Optional[T] {
 	return Optional[T]{hasValue: false}
 }
 
+func FromValueOrFalse[T any](value T, ok bool) Optional[T] {
+	if ok {
+		return Some(value)
+	} else {
+		return None[T]()
+	}
+}
+
+func FromValueOrError[T any](value T, err error) Optional[T] {
+	if err != nil {
+		return Some(value)
+	} else {
+		return None[T]()
+	}
+}
+
 func (o Optional[T]) Value() (value T, ok bool) {
 	return o.value, o.hasValue
 }
@@ -34,10 +50,67 @@ func (o Optional[T]) ValueOrElse(fallback T) T {
 	}
 }
 
+func (o Optional[T]) ValueOrError(ifNone error) (T, error) {
+	if v, ok := o.Value(); ok {
+		return v, nil
+	} else {
+		// Reuse the default value of v created by Value()
+		return v, ifNone
+	}
+}
+
+func (o Optional[T]) ValueOrPanic(message string) T {
+	if v, ok := o.Value(); ok {
+		return v
+	} else {
+		panic(message)
+	}
+}
+
 func (o Optional[_]) IsPresent() bool {
 	return o.hasValue
 }
 
 func (o Optional[_]) IsEmpty() bool {
 	return !o.hasValue
+}
+
+func (o Optional[T]) OrElse(alternative Optional[T]) Optional[T] {
+	if o.IsPresent() {
+		return o
+	} else {
+		return alternative
+	}
+}
+
+func Map[T any, R any](o Optional[T], mapping func(value T) R) Optional[R] {
+	if v, ok := o.Value(); ok {
+		return Some[R](mapping(v))
+	} else {
+		return None[R]()
+	}
+}
+
+func FlatMap[T any, R any](o Optional[T], mapping func(value T) Optional[R]) Optional[R] {
+	if v, ok := o.Value(); ok {
+		return mapping(v)
+	} else {
+		return None[R]()
+	}
+}
+
+func Flatten[T any](o Optional[Optional[T]]) Optional[T] {
+	if v, ok := o.Value(); ok {
+		return v
+	} else {
+		return None[T]()
+	}
+}
+
+func FilterBy[T any](o Optional[T], predicate func(value T) bool) Optional[T] {
+	if v, ok := o.Value(); ok && predicate(v) {
+		return o
+	} else {
+		return None[T]()
+	}
 }
