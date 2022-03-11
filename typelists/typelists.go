5b48761e9e01f0dcb77de2d8cd3448f82cec9902
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-11 14:05:17 +0100

    Update typelists.go

--- typelists/typelists.go
+++ typelists/typelists.go
@@ -1,9 +1,13 @@
 // Package typelists allows representing list of types on the type level.
 package typelists
 
+// A type whose existence only matters for type-level operations,
+// and whose value-level representation is simply empty.
+type phantomType = struct{]
+
 // TypeList can be used as a constraint for type-level lists.
 type TypeList interface{
- 	typeList() /// marker method
+ 	typeList() // marker method
 }
 
 // NonEmpty can be used as a constraint for non-empty type-level lists.
@@ -17,10 +21,10 @@ func (_ Cons[_, _]) typeList() {}
 func (_ Cons[_, _]) nonEmpty() {}
 
 // Nil represents the empty list.
-type Nil struct{}
+type Nil phantomType
 
 // Cons represents a list with a head element and a tail list.
-type Cons[Head any, Tail TypeList] struct{}
+type Cons[Head any, Tail TypeList] phantomType
 
 func (_ Cons[Head, _]) Head() Head { return *new(Head) }
 func (_ Cons[_, Tail]) Tail() Tail { return *new(Tail) }
