5bc86e3b32c82585a2907eff427b2614cf2a8a9c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-12 16:15:52 +0100

    Update math.go

--- funcs/math.go
+++ funcs/math.go
@@ -8,6 +8,22 @@ type Numeric interface {
 	constraints.Complex | constraints.Integer | constraints.Float
 }
 
+func Add[T Numeric | ~string](x T, y T) T {
+	return x + y
+}
+
+func Sub[T Numeric](x T, y T) T {
+	return x - y
+}
+
+func Mul[T Numeric](x T, y T) T {
+	return x * y
+}
+
+func Div[T Numeric](x T, y T) T {
+	return x / y
+}
+
 func Plus[A Numeric | ~string](summand A) func(in A) A {
 	return func(in A) A {
 		return in + summand
