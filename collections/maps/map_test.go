253caec037731313f6c011629fd6125314e3d51b
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-12 14:37:41 +0100

    Update map_test.go

--- collections/maps/map_test.go
+++ collections/maps/map_test.go
@@ -1,8 +1,12 @@
 package maps
 
+import (
+	"github.com/cr7pt0gr4ph7/functional-go/collections/views"
+)
+
 // Ensure that all expected interfaces are implemented.
 func _[K comparable, V any]() {
 	var m Map[K, V]
-	var _ Keyed[K, V] = m
-	var _ Sized = m
+	var _ views.Keyed[K, V] = m
+	var _ views.Sized = m
 }
