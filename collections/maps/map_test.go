93c3c76d1801f413acdaceb8f7a6bd2bbd2bb016
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-12 15:18:26 +0100

    Update map_test.go

--- collections/maps/map_test.go
+++ collections/maps/map_test.go
@@ -1,6 +1,7 @@
 package maps
 
 import (
+	"github.com/cr7pt0gr4ph7/functional-go/collections/builders"
 	"github.com/cr7pt0gr4ph7/functional-go/collections/views"
 )
 
@@ -9,4 +10,6 @@ func _[K comparable, V any]() {
 	var m Map[K, V]
 	var _ views.Keyed[K, V] = m
 	var _ views.Sized = m
+	var _ builders.Keyed[K, V] = m
+	var _ builders.Unordered[Entry[K, V]] = m
 }
