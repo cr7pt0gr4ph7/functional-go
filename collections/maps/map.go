81a12360a314040183c8606e2e9ede612cf7a52c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-12 15:04:15 +0100

    Update map.go

--- collections/maps/map.go
+++ collections/maps/map.go
@@ -2,6 +2,15 @@ package maps
 
 type Map[K comparable, V any] map[K]V
 
+type Entry[K any, V any] struct {
+	Key   K
+	Value V
+}
+
+type Overwrite[K comparable, V any] struct {
+	Map[K, V]
+}
+
 func (m Map[K, V]) At(key K) V {
 	return m[key]
 }
@@ -34,4 +43,48 @@ func (m Map[_, _]) Empty() bool {
 
 func (m Map[_, _]) Len() int {
 	return len(m)
+}
+
+func (m Map[K, V]) Add(entry Entry[K, V]) bool {
+	if !m.DefinedAt(entry.Key) {
+		m[entry.Key] = entry.Value
+		return true
+	}
+	return false
+}
+
+func (o Overwrite[K, V]) Add(entry Entry[K, V]) bool {
+	o.Map.Set(entry)
+	return true
+}
+
+func (m Map[K, V]) Set(entry Entry[K, V]) {
+	m[entry.Key] = entry.Value
+}
+
+func (m Map[K, V]) Remove(entry Entry[K, V]) bool {
+	_, existed := m[entry.Key]
+	// TODO(lw) Compare v and entry.Value?
+	delete(m, entry.Key)
+	return existed
+}
+
+func (m Map[K, V]) InsertAt(key K, value V) {
+	if !m.DefinedAt(key) {
+		m[key] = value
+	}
+}
+
+func (o Overwrite[K, V]) InsertAt(key K, value V) {
+	o.Map.ReplaceAt(key, value)
+}
+
+func (m Map[K, V]) ReplaceAt(key K, value V) {
+	m[key] = value
+}
+
+func (m Map[K, V]) RemoveAt(key K) bool {
+	_, existed := m[key]
+	delete(m, key)
+	return existed
 }
