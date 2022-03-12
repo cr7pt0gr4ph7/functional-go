99f0fc7b21e518a323a14936d1fa1c049b52fe2c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-12 14:37:01 +0100

    Update arraylist_test.go

--- collections/lists/arraylist_test.go
+++ collections/lists/arraylist_test.go
@@ -2,7 +2,16 @@ package lists
 
 import (
 	"fmt"
+	"github.com/cr7pt0gr4ph7/functional-go/collections/views"
 )
+
+// Ensure that all expected interfaces are implemented.
+func _[T any]() {
+	var l ArrayList[T]
+	var _ views.Indexed[T] = l
+	var _ views.Keyed[int, T] = l
+	var _ views.Sized = l
+}
 
 func ExampleArrayList() {
 	var l ArrayList[int]
