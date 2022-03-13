26d08c6f7a6183928c2dc7a2ab05c3b688be4652
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-13 12:47:57 +0100

    Update result.go

--- result/result.go
+++ result/result.go
@@ -2,6 +2,7 @@ package result
 
 import (
 	"errors"
+	"fmt"
 )
 
 var nilError = errors.New("no error given")
@@ -42,9 +43,9 @@ func (r Result[T]) Extract() (T, error) {
 	return r.value, r.err
 }
 
-func (r Result[T]) ValueOrPanic() T {
+func (r Result[T]) MustBeValue() T {
 	if r.err != nil {
-		panic(r.err)
+		panic(fmt.Errorf("expected value but got error: %w", r.err))
 	}
 	return r.value
 }
@@ -53,11 +54,51 @@ func (r Result[_]) Error() error {
 	return r.err
 }
 
-func (r Result[T]) ErrorOrPanic() error {
+func (r Result[T]) MustBeError() error {
 	if r.err == nil {
-		panic("not an error")
+		panic("expected error value, got nil")
 	}
 	return r.err
+}
+
+func (r Result[A]) Catch(whenError func(err error) Result[A]) Result[A] {
+	return Catch(r, whenError)
+}
+
+func Catch[A any](r Result[A], whenError func(err error) Result[A]) Result[A] {
+	if _, e := r.Extract(); e == nil {
+		return r
+	} else {
+		return whenError(e)
+	}
+}
+
+func (r Result[A]) Tap(tapAction func(r Result[A])) Result[A] {
+	tapAction(r)
+	return r
+}
+
+func (r Result[A]) PassThru(hijackFunc func(r Result[A]) Result[A]) Result[A] {
+	return hijackFunc(r)
+}
+
+func (r Result[A]) Do(whenOk func(value A)) Result[A] {
+	return Do(r, whenOk)
+}
+
+func Do[A any](r Result[A], whenOk func(value A)) Result[A] {
+	if v, e := r.Extract(); e == nil {
+		whenOk(v)
+	}
+	return r
+}
+
+func Fold[A any, B any](r Result[A], whenOk func(value A) B, whenError func(err error) B) B {
+	if v, e := r.Extract(); e == nil {
+		return whenOk(v)
+	} else {
+		return whenError(e)
+	}
 }
 
 func Map[A any, B any](r Result[A], mapValue func(value A) B) Result[B] {
@@ -79,6 +120,38 @@ func MapError[A any](r Result[A], mapError func(err error) error) Result[A] {
 func FlatMap[A any, B any](r Result[A], mapValue func(value A) Result[B]) Result[B] {
 	if v, e := r.Extract(); e == nil {
 		return mapValue(v)
+	} else {
+		return Error[B](e)
+	}
+}
+
+func Wrap0[R any, ErrT error](f func() (R, ErrT)) func() Result[R] {
+	return func() Result[R] {
+		return From(f())
+	}
+}
+
+func Wrap1[A any, R any, ErrT error](f func(a A) (R, ErrT)) func(a A) Result[R] {
+	return func(a A) Result[R] {
+		return From(f(a))
+	}
+}
+
+func Wrap2[A any, B any, R any, ErrT error](f func(a A, b B) (R, ErrT)) func(a A, b B) Result[R] {
+	return func(a A, b B) Result[R] {
+		return From(f(a, b))
+	}
+}
+
+func Wrap3[A any, B any, C any, R any, ErrT error](f func(a A, b B, c C) (R, ErrT)) func(a A, b B, c C) Result[R] {
+	return func(a A, b B, c C) Result[R] {
+		return From(f(a, b, c))
+	}
+}
+
+func FlatMap2[A any, B any, ErrT error](r Result[A], mapValue func(value A) (B, ErrT)) Result[B] {
+	if v, e := r.Extract(); e == nil {
+		return From(mapValue(v))
 	} else {
 		return Error[B](e)
 	}
