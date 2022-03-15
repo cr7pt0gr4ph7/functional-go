bd29261d906c258cecdf0c369d1342e3fcb41f5d
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-15 19:37:38 +0100

    Update monad.go

--- types/effects/monad.go
+++ types/effects/monad.go
@@ -23,11 +23,11 @@ func (p Pure[_, _]) pureValue() any         { return p.value }
 func (_ Pure[_, _]) getEffect() EffectTag   { panic("pure") }
 func (_ Pure[_, _]) getQueue() evalTreeNode { panic("pure") }
 
-func (_ Cont[_, _, _]) effImplBase()           {}
-func (_ Cont[_, _, _]) isPure() bool           { return false }
-func (_ Cont[_, _, _]) pureValue() any         { panic("not pure") }
-func (c Cont[_, _, _]) getEffect() EffectTag   { return EffectTag(c.effect) }
-func (c Cont[_, _, _]) getQueue() evalTreeNode { return evalTreeNode(c.queue) }
+func (_ Cont[_, _]) effImplBase()           {}
+func (_ Cont[_, _]) isPure() bool           { return false }
+func (_ Cont[_, _]) pureValue() any         { panic("not pure") }
+func (c Cont[_, _]) getEffect() EffectTag   { return EffectTag(c.effect) }
+func (c Cont[_, _]) getQueue() evalTreeNode { return evalTreeNode(c.queue) }
 
 // Generic type.
 type Eff[E any, T any] struct {
@@ -50,8 +50,8 @@ type effIsNotAnEffImpl struct{}
 // even though it embeds an EffImpl.
 func (_ Eff[E, T]) eff(_ effIsNotAnEffImpl) {}
 
-func (_ Pure[E, T]) eff(eff E) T    { panic("marker method") }
-func (_ Cont[E, _, T]) eff(eff E) T { panic("marker method") }
+func (_ Pure[E, T]) eff(eff E) T { panic("marker method") }
+func (_ Cont[E, T]) eff(eff E) T { panic("marker method") }
 
 type Pure[E any, T any] struct {
 	value T
@@ -74,32 +74,26 @@ func RunPureOrFail[E any, T any](e Eff[E, T]) T {
 	}
 }
 
-type Start any
+type ValueFromEffect any
 
-type Union[E any, T any] EffectTag
-
-func inj[E any, T any, L EffectTag](tag L) Union[E, T] {
-	return Union[E, T](tag)
+type Cont[E any, B any] struct {
+	effect EffectTag
+	queue  evalQueue[E, ValueFromEffect, B]
 }
 
-type Cont[E any, A any, B any] struct {
-	effect Union[E, A]
-	queue  evalQueue[E, A, B]
-}
-
-func newCont[E any, A any, B any](effect Union[E, A], queue evalQueue[E, A, B]) Eff[E, B] {
-	return asEff[E, B](Cont[E, A, B]{
+func newCont[E any, B any](effect EffectTag, queue evalQueue[E, ValueFromEffect, B]) Eff[E, B] {
+	return asEff[E, B](Cont[E, B]{
 		effect: effect,
 		queue:  queue,
 	})
 }
 
+func newPureFromEffect[E any, T any](value ValueFromEffect) Eff[E, T] {
+	return newPure[E, T](value.(T))
+}
+
 func injectEffect[E any, T any, L EffectTag](tag L) Eff[E, T] {
-	// We use Start/any as a workaround for not having universal quantification
-	return newCont[E, Start, T](
-		inj[E, Start](tag),
-		liftQ(func(value Start) Eff[E, T] { return newPure[E](value.(T)) }),
-	)
+	return newCont[E, T](tag, liftQ(newPureFromEffect[E, T]))
 }
 
 func Lift[E any, A any, B any](f func(arg A) B) func(arg Eff[E, A]) Eff[E, B] {
@@ -116,12 +110,15 @@ func Map[E any, A any, B any](arg Eff[E, A], f func(arg A) B) Eff[E, B] {
 	switch m := arg.EffImpl.(type) {
 	case Pure[E, A]:
 		return newPure[E](f(m.value))
-	case Cont[E, Start, A]: // hack for missing universal quantification
+	case Cont[E, A]:
 		g := func(arg A) Eff[E, B] {
 			return newPure[E](f(arg))
 		}
 		return newCont(m.effect, composeQ(m.queue, liftQ(g)))
 	default:
+		if !m.isPure() {
+
+		}
 		panic("unreachable")
 	}
 }
@@ -130,7 +127,7 @@ func FlatMap[E any, A any, B any](arg Eff[E, A], f func(arg A) Eff[E, B]) Eff[E,
 	switch m := arg.EffImpl.(type) {
 	case Pure[E, A]:
 		return f(m.value)
-	case Cont[E, Start, A]: // hack for missing universal quantification
+	case Cont[E, A]:
 		return newCont(m.effect, composeQ(m.queue, liftQ(f)))
 	default:
 		panic("unreachable")
