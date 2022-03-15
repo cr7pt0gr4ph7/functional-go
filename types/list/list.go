e59699a58a11c46f319900cae04b77316ce2e9ae
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-15 22:48:43 +0100

    Update list.go

--- types/list/list.go
+++ types/list/list.go
@@ -1,54 +1,75 @@
 package list
 
 // Represents a simple immutable list.
-type List[T any] interface {
-	Push(item T) List[T]
-	Pop() (head T, tail List[T], ok bool)
-	Head() (T, bool)
-	Tail() List[T]
-}
+type List[T any] struct{ *entry[T] }
 
-func Empty[T]() List[T] {
-	return Nil[T]{}
-}
-
-type Nil[T any] struct{}
-
-func (n Nil[T]) Push(item T) List[T] {
-	return Cons[T]{head: item, tail: n}
-}
-
-func (_ Nil[T]) Pop() (head T, tail List[T], ok bool) {
-	ok = false
-	return
-}
-
-func (_ Nil[T]) Head() (_ T, ok bool) {
-	ok = false
-	return
-}
-
-func (_ Nil[T]) Tail() List[T] {
-	return nil
-}
-
-type Cons[T any] struct {
+type entry[T any] struct {
 	head T
-	tail List[T]
+	tail *entry[T]
 }
 
-func (c Cons[T]) Push(item T) List[T] {
-	return Cons[T]{head: item, tail: c}
+func Empty[T any]() List[T] {
+	return List[T]{}
 }
 
-func (c Cons[T]) Pop() (head T, tail List[T], ok bool) {
-	return c.head, c.tail, true
+func New[T any](s ...T) List[T] {
+	var r List[T]
+	for i := len(s) - 1; i >= 0; i-- {
+		r = r.Push(s[i])
+	}
+	return r
 }
 
-func (c Cons[T]) Head() (_ T, ok bool) {
-	return c.head, true
+func NewReverse[T any](s ...T) List[T] {
+	var r List[T]
+	for _, x := range s {
+		r = r.Push(x)
+	}
+	return r
 }
 
-func (c Cons[T]) Tail() List[T] {
-	return c.tail
+func (l *entry[T]) Prepend(item T) List[T] {
+	return List[T]{&entry[T]{head: item, tail: l}}
+}
+
+func (l *entry[T]) Push(item T) List[T] {
+	return List[T]{&entry[T]{head: item, tail: l}}
+}
+
+func (l *entry[T]) Pop() (head T, tail List[T], ok bool) {
+	if l == nil {
+		ok = false
+		return
+	} else {
+		return l.head, List[T]{l.tail}, true
+	}
+}
+
+func (l *entry[T]) Empty() bool {
+	return l == nil
+}
+
+func (l *entry[T]) Len() int {
+	i := 0
+	for ; l != nil; l = l.tail {
+		i++
+	}
+	return i
+}
+
+func (l *entry[T]) Head() (_ T, ok bool) {
+	if l == nil {
+		ok = false
+		return
+	} else {
+		return l.head, true
+	}
+}
+
+func (l *entry[T]) Tail() List[T] {
+	if l == nil {
+		return List[T]{nil}
+	} else {
+		return List[T]{l.tail}
+	}
 }
