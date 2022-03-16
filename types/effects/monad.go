1dbb7fb0b845e5071fbb9d968d505d20d2d159ac
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-16 13:48:05 +0100

    Update monad.go

--- types/effects/monad.go
+++ types/effects/monad.go
@@ -2,28 +2,23 @@ package effects
 
 // Non-generic workaround.
 type effBase interface {
-	effBase()
 	impl() effImplBase
 }
 
 type effImplBase interface {
-	effImplBase()
 	isPure() bool
 	pureValue() any
 	getEffect() EffectTag
 	getQueue() evalTreeNode
 }
 
-func (_ Eff[_, _]) effBase()          {}
 func (e Eff[_, _]) impl() effImplBase { return e.EffImpl }
 
-func (_ Pure[_, _]) effImplBase()           {}
 func (_ Pure[_, _]) isPure() bool           { return true }
 func (p Pure[_, _]) pureValue() any         { return p.value }
 func (_ Pure[_, _]) getEffect() EffectTag   { panic("pure") }
 func (_ Pure[_, _]) getQueue() evalTreeNode { panic("pure") }
 
-func (_ Cont[_, _]) effImplBase()           {}
 func (_ Cont[_, _]) isPure() bool           { return false }
 func (_ Cont[_, _]) pureValue() any         { panic("not pure") }
 func (c Cont[_, _]) getEffect() EffectTag   { return EffectTag(c.effect) }
@@ -74,31 +69,38 @@ func RunPureOrFail[E any, T any](e Eff[E, T]) T {
 	}
 }
 
-type ValueFromEffect any
+type valueFromEffect any
 
 type Cont[E any, B any] struct {
 	effect EffectTag
-	queue  evalQueue[E, ValueFromEffect, B]
+	queue  evalRightNode[E, B]
 }
 
-func newCont[E any, B any](effect EffectTag, queue evalQueue[E, ValueFromEffect, B]) Eff[E, B] {
+func newCont[E any, A any, B any, L TypedEffectTag[A]](effect L, queue evalQueue[E, A, B]) Eff[E, B] {
 	return asEff[E, B](Cont[E, B]{
 		effect: effect,
 		queue:  queue,
 	})
 }
 
-func newPureFromEffect[E any, T any](value ValueFromEffect) Eff[E, T] {
-	return newPure[E, T](value.(T))
+func injectEffect[E any, T any, L TypedEffectTag[T]](tag L) Eff[E, T] {
+	return newCont(tag, passThruQ[E, T]())
 }
 
-func injectEffect[E any, T any, L EffectTag](tag L) Eff[E, T] {
-	return newCont[E, T](tag, liftQ(newPureFromEffect[E, T]))
+func newContUnchecked[E any, B any](effect EffectTag, queue evalRightNode[E, B]) Eff[E, B] {
+	return asEff[E, B](Cont[E, B]{
+		effect: effect,
+		queue:  queue,
+	})
+}
+
+func injectEffectUnchecked[E any, T any, L EffectTag](tag L) Eff[E, T] {
+	return newContUnchecked[E, T](tag, passThruQ[E, T]())
 }
 
 func Lift[E any, A any, B any](f func(arg A) B) func(arg Eff[E, A]) Eff[E, B] {
 	return func(arg Eff[E, A]) Eff[E, B] {
-		return Return[E](f(arg.EffImpl.(Pure[E, A]).value))
+		return Map(arg, f)
 	}
 }
 
@@ -114,11 +116,8 @@ func Map[E any, A any, B any](arg Eff[E, A], f func(arg A) B) Eff[E, B] {
 		g := func(arg A) Eff[E, B] {
 			return newPure[E](f(arg))
 		}
-		return newCont(m.effect, composeQ(m.queue, liftQ(g)))
+		return newContUnchecked(m.effect, composeQ(m.queue, liftQ(g, "Map")))
 	default:
-		if !m.isPure() {
-
-		}
 		panic("unreachable")
 	}
 }
@@ -128,7 +127,7 @@ func FlatMap[E any, A any, B any](arg Eff[E, A], f func(arg A) Eff[E, B]) Eff[E,
 	case Pure[E, A]:
 		return f(m.value)
 	case Cont[E, A]:
-		return newCont(m.effect, composeQ(m.queue, liftQ(f)))
+		return newContUnchecked(m.effect, composeQ(m.queue, liftQ(f, "FlatMap")))
 	default:
 		panic("unreachable")
 	}
@@ -143,21 +142,31 @@ func Chain[E any, A any, Discard any](first Eff[E, A], others ...Eff[E, Discard]
 	return r
 }
 
+func ReplaceResult[E any, A any, B any](e Eff[E, A], newResult B) Eff[E, B] {
+	return Map(e, func(_ A) B { return newResult }).
+		WithDebugTag("ReplaceResult", newResult)
+}
+
 // Discards the value but keeps the effects.
 func (e Eff[E, A]) Discard() Eff[E, Unit] {
-	return Map(e, func(_ A) Unit { return Unit{} })
+	return ReplaceResult(e, UnitValue).
+		WithDebugTag("Discard")
 }
 
 // Appends the effects from other, but keeps the current value from e.
 func (e Eff[E, A]) And(other Eff[E, Unit]) Eff[E, A] {
 	return FlatMap(e, func(arg A) Eff[E, A] {
-		return Map(other, func(_ Unit) A { return arg })
-	})
+		return ReplaceResult(other, arg)
+	}).WithDebugTag("And")
 }
 
 // Appends the effects returned by f, but keeps the current value from e.
 func (e Eff[E, A]) Then(f func(arg A) Eff[E, Unit]) Eff[E, A] {
 	return FlatMap(e, func(arg A) Eff[E, A] {
-		return Map(f(arg), func(_ Unit) A { return arg })
-	})
+		return ReplaceResult(f(arg), arg)
+	}).WithDebugTag("Then")
+}
+
+func (e Eff[E, A]) WithDebugTag(debugTag string, debugInfo ...any) Eff[E, A] {
+	return e
 }
