b9a1fddb5ace66e6a61fea01048237b463e0dba8
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-17 13:03:19 +0100

    Update hlist.go

--- collections/heterogeneous/hlist/hlist.go
+++ collections/heterogeneous/hlist/hlist.go
@@ -12,6 +12,9 @@ type HList interface {
 	Len() int
 	countTailRec(count int) int
 	Slice() []any
+	String() string
+	Each(f func(item any))
+	EachWithIndex(f func(index int, item any))
 }
 
 // NonEmpty can be used as a constraint for non-empty heterogeneous lists.
@@ -27,20 +30,35 @@ func (_ Cons[_, _]) nonEmpty() {}
 func (_ Nil) hExtract() (any, HList, bool)        { return nil, nil, false }
 func (c Cons[H, T]) hExtract() (any, HList, bool) { return c.Head, c.Tail, true }
 
+// =========
+// :: Nil ::
+// =========
+
 // Nil represents the empty list.
 type Nil struct{}
 
-func (_ Nil) Slice() (_ []any)           { return }
-func (_ Nil) String() string             { return "Nil" }
 func (_ Nil) Empty() bool                { return true }
 func (_ Nil) Len() int                   { return 0 }
 func (_ Nil) countTailRec(count int) int { return count }
+func (_ Nil) Slice() (_ []any)           { return }
+func (_ Nil) String() string             { return "Nil" }
+
+func (_ Nil) Each(f func(item any))                     {}
+func (_ Nil) EachWithIndex(f func(index int, item any)) {}
+
+// ==========
+// :: Cons ::
+// ==========
 
 // Cons represents a list with a head element and a tail list.
 type Cons[Head any, Tail HList] struct {
 	Head Head
 	Tail Tail
 }
+
+func (_ Cons[_, _]) Empty() bool                { return false }
+func (c Cons[_, _]) Len() int                   { return c.countTailRec(0) }
+func (c Cons[_, _]) countTailRec(count int) int { return c.Tail.countTailRec(1 + count) }
 
 func (c Cons[_, _]) Slice() []any {
 	return append([]any{c.Head}, c.Tail.Slice()...)
@@ -50,12 +68,51 @@ func (c Cons[H, T]) String() string {
 	return fmt.Sprintf("%v ::: %v", c.Head, c.Tail)
 }
 
-func (_ Cons[_, _]) Empty() bool                { return false }
-func (c Cons[_, _]) Len() int                   { return c.countTailRec(0) }
-func (c Cons[_, _]) countTailRec(count int) int { return c.Tail.countTailRec(1 + count) }
+func (c Cons[H, T]) Each(f func(item any)) {
+	for h, t, ok := c.hExtract(); ok; h, t, ok = t.hExtract() {
+		f(h)
+	}
+}
+
+func (c Cons[H, T]) EachWithIndex(f func(index int, item any)) {
+	i := 0
+	for h, t, ok := c.hExtract(); ok; h, t, ok = t.hExtract() {
+		f(i, h)
+		i++
+	}
+}
+
+// =============
+// :: Methods ::
+// =============
 
 func Prepend[H any, T HList](head H, tail T) Cons[H, T] {
 	return Cons[H, T]{Head: head, Tail: tail}
+}
+
+func FoldLeft[L HList, R any](l L, initial R, foldFn func(item any, acc R) R) R {
+	acc := initial
+	for h, t, ok := l.hExtract(); ok; h, t, ok = t.hExtract() {
+		acc = foldFn(h, acc)
+	}
+	return acc
+}
+
+func MapFoldLeft[L HList, T any, R any](l L, initial R, mapFn func(item any) T, foldFn func(item T, acc R) R) R {
+	acc := initial
+	for h, t, ok := l.hExtract(); ok; h, t, ok = t.hExtract() {
+		acc = foldFn(mapFn(h), acc)
+	}
+	return acc
+}
+
+func MapReduceLeft[L NonEmpty, R any](l L, mapFn func(item any) R, reduceFn func(item R, acc R) R) R {
+	h, t, ok := l.hExtract()
+	acc := mapFn(h)
+	for h, t, ok = l.hExtract(); ok; h, t, ok = t.hExtract() {
+		acc = reduceFn(mapFn(h), acc)
+	}
+	return acc
 }
 
 func UnsafeSliceOf[A any, H any, T HList](l Cons[H, T]) []A {
