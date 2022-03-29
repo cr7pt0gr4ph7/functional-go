564919b720ffabe36d41ae3d2bad2d7b5087620c
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2022-03-29 11:38:37 +0200

    Update interfaces.go

--- collections/interfaces.go
+++ collections/interfaces.go
@@ -1,6 +1,35 @@
-package builders
+package collections
 
-type Indexed[T any] Keyed[int, T]
+type Sized interface {
+	Empty() bool
+	Len() int
+}
+
+type ReadOnlyKeyed[K any, V any] interface {
+	At(key K) V
+	AtOrDefault(key K) V
+	AtOrElse(key K, fallback V) V
+	TryAt(key K) (value V, ok bool)
+	DefinedAt(key K) bool
+}
+
+type ReadOnlyIndexed[T any] ReadOnlyKeyed[int, T]
+
+// ===================
+// Mutable collections
+// ===================
+
+// Represents a mutable collection with the ability to add elements.
+type Builder[T any] interface {
+	Add(elem T) bool
+}
+
+// Represents a mutable collection or set.
+type Unordered[T any] interface {
+	Add(elem T) bool    // Add `elem` to the collection. Returns `true` when the element was actually added.
+	Remove(elem T) bool // Removes `elem` from the collection. Returns `true` when the element was actually removed.
+	Clear()             // Removes all elements from the collection.
+}
 
 type Keyed[K any, V any] interface {
 	InsertAt(key K, value V)
@@ -9,12 +38,4 @@ type Keyed[K any, V any] interface {
 	Clear()
 }
 
-type Builder[T any] interface {
-	Add(elem T) bool
-}
-
-type Unordered[T any] interface {
-	Add(elem T) bool
-	Remove(elem T) bool
-	Clear()
-}
+type Indexed[T any] Keyed[int, T]
